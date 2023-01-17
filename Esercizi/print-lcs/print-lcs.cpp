753e6a4dd4117a2533f9010edbfbb86c20bf1229
Giulio Pedicone <pediconegiulio02@gmail.com>
2023-01-17 11:21:10 +0100

    Add print-lcs

--- Esercizi/print-lcs/print-lcs.cpp
+++ Esercizi/print-lcs/print-lcs.cpp
@@ -6,32 +6,47 @@ using namespace std;
  * Nota Bene: Se il vostro PC genera meno di 100 elementi eseguire il codice con https://www.online-cpp.com/
  */
 
-int lcs(string X, string Y, int m, int n)
+string lcs(string X, string Y, int m, int n)
 {
     int L[m + 1][n + 1];
-    int i, j;
 
-    for (i = 0; i <= m; i++)
+    for (int i = 0; i <= m; i++)
     {
-        for (j = 0; j <= n; j++)
+        for (int j = 0; j <= n; j++)
         {
             if (i == 0 || j == 0)
                 L[i][j] = 0;
-
             else if (X[i - 1] == Y[j - 1])
                 L[i][j] = L[i - 1][j - 1] + 1;
-
             else
                 L[i][j] = max(L[i - 1][j], L[i][j - 1]);
         }
     }
 
-    return L[m][n];
-}
+    int index = L[m][n];
 
-int max(int a, int b)
-{
-    return (a > b) ? a : b;
+    char lcs[index + 1];
+    lcs[index] = '\0';
+
+    int i = m, j = n;
+
+    while (i > 0 && j > 0)
+    {
+        if (X[i - 1] == Y[j - 1])
+        {
+            lcs[index - 1] = X[i - 1];
+            i--;
+            j--;
+            index--;
+        }
+
+        else if (L[i - 1][j] > L[i][j - 1])
+            i--;
+        else
+            j--;
+    }
+
+    return lcs;
 }
 
 int main()
